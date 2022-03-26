f8da2ae92f8da49759e7b06d58dd82e4ede759a5
pedromonteiro01 <pmapm@ua.pt>
2022-03-26 13:02:56 +0000

    navbar now working

--- scripts/show-navbar-icon.js
+++ scripts/show-navbar-icon.js
@@ -5,3 +5,20 @@ $(window).on("scroll", function () {
         $('#navbar-icon').fadeOut(100);
     }
  });
+
+
+var navbarIcon = document.getElementById("navbar-icon");
+var navbarAreaScroll = document.getElementById("navbar-scroll");
+
+navbarIcon.onclick = function (){
+    if (navbarAreaScroll.style.display == "none")
+        navbarAreaScroll.style.display = "flex";
+    else
+        navbarAreaScroll.style.display = "none";
+}
+
+var navbarLink = document.getElementById("nav-link");
+navbarLink.onclick = function () {
+    navbarAreaScroll.style.display = "none";
+    console.log("hi");
+}
