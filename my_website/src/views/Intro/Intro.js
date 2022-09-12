9aac7f92dd7ab70479390e39e2f15306c3e5c735
pedromonteiro01 <pmapm@ua.pt>
2022-09-12 15:08:04 +0100

    add scroll down button

--- my_website/src/views/Intro/Intro.js
+++ my_website/src/views/Intro/Intro.js
@@ -1,11 +1,13 @@
 import ParticlesBackground from "../../components/ParticlesBackground/ParticlesBackground"
 import Header from '../../components/Header/Header';
+import ScrollDownButton from "../../components/ScrollDownButton/ScrollDownButton";
 
 const Intro = () => {
     return (
         <>
         <ParticlesBackground />
         <Header />
+        <ScrollDownButton />
         </>
     )
 }
