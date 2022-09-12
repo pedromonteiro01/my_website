b170e13d95049c40f965389743b964bf58836b99
pedromonteiro01 <pmapm@ua.pt>
2022-09-12 14:18:44 +0100

    ADD REACT TYPING ANIMATION

--- my_website/src/views/MyBackground/components/Skills.js
+++ my_website/src/views/MyBackground/components/Skills.js
@@ -1,5 +1,6 @@
 import "./Skills.css";
 import { SkillsData } from "./skills-data.js";
+import { TypeAnimation } from 'react-type-animation';
 
 const Skills = () => {
   return (
@@ -8,7 +9,49 @@ const Skills = () => {
         <img src="img/code.png" alt="" />
         <p className="main-p">Skills</p>
         <p className="element-p">
-          I've worked with <span className="element"></span>
+          I've worked with <span className="element">    
+      <TypeAnimation
+      sequence={[
+        'Python',
+        1000,
+        'Java',
+        2000,
+        'C/C++',  
+        3000,
+        'HTML/CSS',  
+        4000,
+        'Javascript',  
+        5000,
+        'ReactJS',  
+        6000,
+        'PHP',  
+        7000,
+        'Spring Boot',  
+        8000,
+        'Django',  
+        9000,
+        'C#', 
+        10000,
+        'SQL',  
+        11000,
+        'MYSQL',  
+        12000,
+        'Docker',  
+        13000,
+        'Flutter',  
+        14000,
+        'Dart',  
+        15000,
+        () => {
+          console.log('Done typing!'); // Place optional callbacks anywhere in the array
+        }
+      ]}
+      speed={1}
+      wrapper="span"
+      cursor={true}
+      repeat={Infinity}
+      style={{ fontSize: '16px' }}
+    /></span>
         </p>
       </div>
       <ul>
