58b69b47f34fb633b416848b65d8ed2c554ffc80
Pedro Monteiro <pmapm@ua.pt>
2023-03-04 12:44:45 +0000

    fix lateral navbar

--- src/components/Navbar/components/FullNavbar.js
+++ src/components/Navbar/components/FullNavbar.js
@@ -6,7 +6,8 @@ const FullNavbar = () => {
         <ul>
             <li><a href="#">Home</a></li>         
             <li><a href="#about">About</a></li>    
-            <li><a href="#background">My Background</a></li>           
+            <li><a href="#background">My Background</a></li>   
+            <li><a href="#experience">Experience</a></li>          
             <li><a href="#project">Projects</a></li>           
             <li><a href="#contacts">Contact</a></li>            
         </ul>
