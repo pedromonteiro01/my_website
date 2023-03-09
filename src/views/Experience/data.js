a7490b9f79828e85be9873e853fd52f63dfd7e16
Pedro Monteiro <pmapm@ua.pt>
2023-03-09 17:32:59 +0000

    minor fix

--- src/views/Experience/data.js
+++ src/views/Experience/data.js
@@ -1,40 +1,59 @@
-import { data } from "./data";
-import ExperienceCard from "../../components/ExperienceCard/ExperienceCard";
-import "animate.css/animate.min.css";
-import { AnimationOnScroll } from 'react-animation-on-scroll';
+import { FaStar } from "react-icons/fa";
 
-const Experience = () => {
-  return (
-    <div className="timeline" id="experience">
-      <h3 id="experienceHeader">Experience</h3>
-      <div className="wrapper" id="wrapper">
-        <div className="center-line">
-          <a href="#experienceHeader" class="scroll-icon">
-            <i class="fas fa-caret-up"></i>
-          </a>
-        </div>
-        {data.map((item) => {
-          return (
-            <>
-            <AnimationOnScroll animateOnce={true} animateIn="animate__bounceIn">
-            <ExperienceCard
-              icon={item.icon}
-              key={item.id}
-              class={item.class}
-              title={item.title}
-              date={item.date}
-              text={item.text}
-              local={item.local}
-              link={item.link}
-              img={item.img}
-            />
-            </AnimationOnScroll>
-            </>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
-
-export default Experience;
+export const data = [
+  {
+    id: 1,
+    icon: <FaStar />,
+    class: "row row-1",
+    title: "Research & Development",
+    date: "Jul 2022 - Present",
+    text: "After successfully concluding my bachelor's degree final project with a classification of 19/20, I was invited, along with my colleagues, to continue to work on this project with the intent to make it closer to a production-ready product, set to be used by the university's community.",
+    local: "Aveiro University",
+    link: "https://www.ua.pt/",
+    img: "/img/aveiro.png",
+  },
+  {
+    id: 2,
+    icon: <FaStar />,
+    class: "row row-2",
+    title: "Internal Management Member",
+    date: "Nov 2021 - Present",
+    text: "Responsible for maintaining and creating new features on the course website, working with technologies such as ReactJS, PHP and MySQL.",
+    local: "Núcleo de Estudantes de Informática AAUAV",
+    link: "https://nei.web.ua.pt/",
+    img: "/img/nei.png",
+  },
+  {
+    id: 3,
+    icon: <FaStar />,
+    class: "row row-1",
+    title: "Business Relations Member",
+    date: "Mar 2022 - Oct 2022",
+    text: "Contact companies, speakers and individual entities in order to organize events. Also connected to the IT part, responsible for the website development, using ReactJS and GraphQL.",
+    local: "Scientific Junior Value",
+    link: "https://www.scientific.pt/",
+    img: "/img/scientifi.png",
+  },
+  {
+    id: 4,
+    icon: <FaStar />,
+    class: "row row-2",
+    title: "External Collaborator",
+    date: "Oct 2019 - Nov 2021",
+    text: "Helping with tasks, such as organizing events, games or student meetings.",
+    local: "Núcleo de Estudantes de Informática AAUAV",
+    link: "https://nei.web.ua.pt/",
+    img: "/img/nei.png",
+  },
+  {
+    id: 5,
+    icon: <FaStar />,
+    class: "row row-1",
+    title: "External Collaborator",
+    date: "Dec 2019 - Fev 2020",
+    text: "",
+    local: "Aveiro University",
+    link: "https://www.ua.pt/",
+    img: "/img/aveiro.png",
+  },
+];
