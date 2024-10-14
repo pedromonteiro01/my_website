69e286695fa298230d322d87a89f6b74223d95f5
Pedro Monteiro <pmapm@ua.pt>
2024-10-14 22:58:18 +0100

    change education timeline

--- src/views/MyBackground/components/education-data.js
+++ src/views/MyBackground/components/education-data.js
@@ -1,18 +1,21 @@
 export const EducationData = [
   {
     id: 1,
+    class: "right",
     img: "/img/aveiro.png",
     text: "Computer Science Engineering MSc Degree",
     date: "Sep 2022 - Present",
   },
   {
     id: 2,
+    class: "left",
     img: "/img/aveiro.png",
     text: "Computer Science Engineering BSc Degree",
     date: "Sep 2019 - Jul 2022",
   },
   {
     id: 3,
+    class: "right",
     img: "/img/aese.jpg",
     text: "Scientific High School Degree",
     date: "Sep 2016 - Jul 2019",
