b5cbf33de9991ada2d1f310af6415d3f13cfb831
Pedro Monteiro <pmapm@ua.pt>
2024-10-15 22:53:58 +0100

    change timeline

--- src/views/Experience/Experience.js
+++ src/views/Experience/Experience.js
@@ -9,28 +9,34 @@ const Experience = () => {
       <h3 id="experienceHeader">Experience</h3>
       <div className="wrapper" id="wrapper">
         <div className="center-line">
-          <a href="#experienceHeader" class="scroll-icon">
-            <i class="fas fa-caret-up"></i>
+          <a href="#experienceHeader" className="scroll-icon">
+            <i className="fas fa-caret-up"></i>
           </a>
         </div>
-        {data.map((item) => {
+        {data.map((item, index) => {
+          // Alternate animation effects for rows
+          const animationEffect = index % 2 === 0 ? "animate__fadeInLeft" : "animate__fadeInRight";
+          const animationDuration = "animate__animated " + animationEffect; // Slow effect
+
           return (
-            <>
-            <AnimationOnScroll animateOnce={true} animateIn={item.animation}>
-            <ExperienceCard
-              icon={item.icon}
+            <AnimationOnScroll
+              animateOnce={true}
+              animateIn={animationDuration}
+              delay={index * 30}
               key={item.id}
-              class={item.class}
-              title={item.title}
-              date={item.date}
-              text={item.text}
-              local={item.local}
-              link={item.link}
-              img={item.img}
-              techStack={item.techStack}
-            />
+            >
+              <ExperienceCard
+                icon={item.icon}
+                class={item.class}
+                title={item.title}
+                date={item.date}
+                text={item.text}
+                local={item.local}
+                link={item.link}
+                img={item.img}
+                techStack={item.techStack}
+              />
             </AnimationOnScroll>
-            </>
           );
         })}
       </div>
