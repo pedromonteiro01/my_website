9ff99037d545abc82367368d5405f6c35862839c
Pedro Monteiro <pmapm@ua.pt>
2024-10-16 12:06:49 +0100

    fix navbar redirecting

--- src/components/Navbar/components/FullNavbar.js
+++ src/components/Navbar/components/FullNavbar.js
@@ -1,19 +1,32 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './FullNavbar.css';
 
 const FullNavbar = () => {
   const location = useLocation();
   const isProjectsPage = location.pathname === "/projects" || location.pathname === "/projects/";
 
+  const handleNavigation = (event, section) => {
+    // Prevent the default behavior of the anchor tag
+    event.preventDefault();
+    
+    // If on the projects page, change the URL to root with the hash
+    if (isProjectsPage) {
+      window.location.href = `/#${section}`;
+    } else {
+      // If not on the projects page, simply scroll to the section
+      window.location.href = `#${section}`;
+    }
+  };
+
   return (
     <div className={`banner-text ${isProjectsPage ? 'projects-navbar' : ''}`}>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/#about">About</Link></li>
-        <li><Link to="/#background">My Background</Link></li>
-        <li><Link to="/#experience">Experience</Link></li>
-        <li><Link to="/#projects">Projects</Link></li>
-        <li><Link to="/#contacts">Contact</Link></li>
+        <li><a href="/" onClick={(e) => handleNavigation(e, "")}>Home</a></li>
+        <li><a href="#about" onClick={(e) => handleNavigation(e, "about")}>About</a></li>
+        <li><a href="#background" onClick={(e) => handleNavigation(e, "background")}>My Background</a></li>
+        <li><a href="#experience" onClick={(e) => handleNavigation(e, "experience")}>Experience</a></li>
+        <li><a href="#projects" onClick={(e) => handleNavigation(e, "projects")}>Projects</a></li>
+        <li><a href="#contacts" onClick={(e) => handleNavigation(e, "contacts")}>Contact</a></li>
       </ul>
     </div>
   );
